2692429000ffb063bead990f8e8f548adef86659
Nickghost inc <you@example.com>
2018-09-09 12:34:32 +0300

    .config.py updated

---
+++ config.py
@@ -0,0 +1,26 @@
+import os
+
+class Config:
+    '''
+    general configuration
+    '''
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class ProdConfig(Config):
+    '''
+    production configuration child class
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+class DevConfig(Config):
+    '''
+    development configuration child class
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://gitu_m:sqlpass@localhost/pitchsite'
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
