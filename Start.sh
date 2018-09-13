7028e1cb8fbaa0a3a8affe85a42f6d6409ef0f60
Nickghost inc <you@example.com>
2018-09-13 09:33:23 +0300

    changes made

--- Start.sh
+++ Start.sh
@@ -1,4 +1,4 @@
 export SECRET_KEY=iamacarenthusiast
-
+export DATABASE_URL='postgres://komkrhtvhkqhpf:d1f780c5d2f7b58754ef26f4b85a8dbd6934d59cba70f8400ffca2845f5bb1b7@ec2-54-227-247-225.compute-1.amazonaws.com:5432/d6bgqi19ennurv'
 
 python3.6 manage.py server
